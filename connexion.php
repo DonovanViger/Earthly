6bee572447623145937f0488b06eff7f8ea24536
Emma Roizot <emma.roizot@orange.fr>
2024-02-09 10:06:03 +0100

    création page connexion

---
+++ connexion.php
@@ -0,0 +1,9 @@
+<?php
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=sae401-2', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $erreur) {
+    die("Erreur de connexion à la base de données : ". $erreur->getMessage());
+}
+?>
