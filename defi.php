f3694eb7dd6fc96c67390e08ecaacd9c8667dea3
Emma Roizot <emma.roizot@orange.fr>
2024-02-09 10:02:15 +0100

    Ajout affichage défi

--- defi.php
+++ defi.php
@@ -1,9 +1,23 @@
 <?php
-
 try {
+    // Connexion à la base de données
     $db = new PDO('mysql:host=localhost;dbname=sae401-2', 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Requête SQL pour récupérer les défis
+    $requete = $db->query("SELECT * FROM defis ORDER BY ID_Defi");
+
+    // Affichage des défis
+    echo "<h1>Liste des défis</h1>";
+    echo "<ul>";
+    while ($defi = $requete->fetch(PDO::FETCH_ASSOC)) {
+        echo "<li>";
+        echo "<strong>",$defi['nom'],"</strong>: ", $defi['desc'];
+        echo "</li>";
+    }
+    echo "</ul>";
 } catch (PDOException $erreur) {
+    // En cas d'erreur de connexion à la base de données
     die("Erreur de connexion à la base de données : ". $erreur->getMessage());
 }
 ?>
