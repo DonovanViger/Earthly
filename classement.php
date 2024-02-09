a7725c1a2ff8db62a16677717bdac0c8b262dadb
Donovan Viger <donovan.pro@outlook.fr>
2024-02-09 10:33:09 +0100

    ajustement classement.php

--- classement.php
+++ classement.php
@@ -4,37 +4,45 @@ try {
     $db = new PDO('mysql:host=localhost;dbname=sae401-2', 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $erreur) {
-    die("Erreur de connexion à la base de données : ". $erreur->getMessage());
+    die("Erreur de connexion à la base de données : " . $erreur->getMessage());
 }
 
 $requeteClassement = $db->prepare("SELECT `pseudo`,`point_Utilisateur` FROM `utilisateurs` ORDER BY `point_Utilisateur` DESC LIMIT 10;");
 $requeteClassement->execute();
 $Classements = $requeteClassement->fetchAll(PDO::FETCH_ASSOC);
+$i = 0;
 ?>
 
 <html>
 <h2>Classement</h2>
-    <table>
-      <thead>
+<table>
+    <thead>
         <tr>
-          <th scope="col">N°</th>
-          <th scope="col">Speudo</th>
-          <th scope="col">Points</th>
+            <th scope="col">N°</th>
+            <th scope="col">Speudo</th>
+            <th scope="col">Points</th>
         </tr>
-      </thead>
-      <tbody>
+    </thead>
+    <tbody>
         <?php foreach ($Classements as $classement) { ?>
             <?php
-                $i = $i + 1 ?>
-          <tr>
-            <td><?php echo $i?></td>
-            <td><?php echo $classement['pseudo']; ?></td>
-            <td><?php echo $classement['point_Utilisateur']; ?></td>
+            $i = $i + 1 
+            ?>
+            <tr>
+                <td>
+                    <?php echo $i ?>
+                </td>
+                <td>
+                    <?php echo $classement['pseudo']; ?>
+                </td>
+                <td>
+                    <?php echo $classement['point_Utilisateur']; ?>
+                </td>
 
-          </tr>
+            </tr>
         <?php } ?>
-      </tbody>
-    </table>
+    </tbody>
+</table>
 
 
 </html>
