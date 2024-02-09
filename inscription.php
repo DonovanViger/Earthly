89b2a7d412ee5878a5505b12ccbe9d56a56a84fa
Emma Roizot <emma.roizot@orange.fr>
2024-02-09 10:33:46 +0100

    création formulaire inscription

---
+++ inscription.php
@@ -0,0 +1,29 @@
+<?php
+try {
+    // Connexion à la base de données
+    $db = new PDO('mysql:host=localhost;dbname=sae401-2', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Récupération des données du formulaire
+    $pseudo = $_POST['pseudo'];
+    $email = $_POST['email'];
+    $mdp = $_POST['mdp'];
+    $dateCreationCompte = date('Y-m-d');
+
+    // Hacher le mot de passe
+    $motDePasseHache = $mdp
+
+    // Préparer et exécuter la requête d'insertion
+    $requete = $db->prepare("INSERT INTO utilisateurs (pseudo, mail, mdp, point_Utilisateur, dateConnexion, dateCreationCompte, expPlaneteUtilisateur) VALUES (:pseudo, :email, :mdp, 0, :dateCreationCompte, :dateCreationCompte, 0)");
+    $requete->bindParam(':pseudo', $pseudo);
+    $requete->bindParam(':email', $email);
+    $requete->bindParam(':mdp', $motDePasseHache);
+    $requete->bindParam(':dateCreationCompte', $dateCreationCompte);
+    $requete->execute();
+
+    echo "Compte créé avec succès.";
+} catch (PDOException $erreur) {
+    // En cas d'erreur de connexion à la base de données
+    die("Erreur de connexion à la base de données : ". $erreur->getMessage());
+}
+?>
