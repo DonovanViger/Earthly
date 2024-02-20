60eecc84d4e8e35996523e90a7b3d148f671bc08
Baptiste Rouzic <rouzicb@gmail.com>
2024-02-20 15:17:46 +0100

    commit navbar

--- pages/defi.php
+++ pages/defi.php
@@ -35,11 +35,10 @@ try {
 
 if (isset($_SESSION['pseudo'])) {
 ?>
-    <li><a href="pages/planet.php">Ma Planète</a></li>
-    <li><a href="pages/defi.php">Mes défis journaliers</a></li>
-    <li><a href="pages/recyclage.php">Carte des poubelles</a></li>
-    <li><a href="pages/compte.php">Mon compte</a></li>
-    <li><a href="form/deconnexion.php">Se déconnecter</a></li>
+    <li><a href="planet.php">Ma Planète</a></li>
+    <li><a href="defi.php">Mes défis journaliers</a></li>
+    <li><a href="recyclage.php">Carte des poubelles</a></li>
+    <li><a href="compte.php">Mon compte</a></li>
 </ul>
 <?php
 }
--- pages/recyclage.php
+++ pages/recyclage.php
@@ -10,10 +10,10 @@ try {
         // Affiche les liens "Se connecter" et "Créer un compte" seulement si l'utilisateur n'est pas connecté
         if (isset($_SESSION['pseudo'])) {
         ?>
-            <li><a href="pages/planet.php">Ma Planète</a></li>
-            <li><a href="pages/defi.php">Mes défis journaliers</a></li>
-            <li><a href="pages/recyclage.php">Carte des poubelles</a></li>
-            <li><a href="pages/compte.php">Mon compte</a></li>
+            <li><a href="planet.php">Ma Planète</a></li>
+            <li><a href="defi.php">Mes défis journaliers</a></li>
+            <li><a href="recyclage.php">Carte des poubelles</a></li>
+            <li><a href="compte.php">Mon compte</a></li>
     </ul>
 <?php
         }
