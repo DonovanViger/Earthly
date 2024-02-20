a9762a386964d5721f2bdee7fec056b7ce7d4cc3
Baptiste Rouzic <rouzicb@gmail.com>
2024-02-20 15:06:40 +0100

    ajout navbar

--- pages/compte.php
+++ pages/compte.php
@@ -54,5 +54,27 @@ try {
         <li>Niveau d'expérience sur la planète : <?php echo $utilisateur['expPlaneteUtilisateur']; ?></li>
     </ul>
     <a href="../index.php">Retour à l'accueil</a>
+
+    <?php
+        // Affiche les liens "Se connecter" et "Créer un compte" seulement si l'utilisateur n'est pas connecté
+        if (!isset($_SESSION['pseudo'])) {
+        ?>
+            <li><a href="pages/connexion.php">Se connecter</a></li>
+            <li><a href="pages/inscription.php">Créer un compte</a></li>
+        <?php
+        }
+
+        if (isset($_SESSION['pseudo'])) {
+        ?>
+            <li><a href="pages/planet.php">Ma Planète</a></li>
+            <li><a href="pages/defi.php">Mes défis journaliers</a></li>
+            <li><a href="pages/recyclage.php">Carte des poubelles</a></li>
+            <li><a href="pages/compte.php">Mon compte</a></li>
+            <li><a href="form/deconnexion.php">Se déconnecter</a></li>
+    </ul>
+<?php
+        }
+
+?>
 </body>
 </html>
