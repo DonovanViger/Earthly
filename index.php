3c2df1a128eba8ff3d3951aa24c9ce37e5ced2b5
Baptiste Rouzic <rouzicb@gmail.com>
2024-02-20 14:40:14 +0100

    changement index

--- index.php
+++ index.php
@@ -29,13 +29,18 @@ if(isset($_SESSION['pseudo'])) {
             <li><a href="pages/inscription.php">Créer un compte</a></li>
             <?php
         }
-        ?>
+
+        if(isset($_SESSION['pseudo'])) {
+            ?>
         <li><a href="pages/planet.php">Ma Planète</a></li>
         <li><a href="pages/defi.php">Mes défis journaliers</a></li>
         <li><a href="pages/recyclage.php">Carte des poubelles</a></li>
         <li><a href="pages/compte.php">Mon compte</a></li>
+        <li><a href="form/deconnexion.php">Se déconnecter</a></li>
     </ul>
+    <?php
+        }
     
-    <a href="form/deconnexion.php">Se déconnecter</a>
+        ?>
 </body>
 </html>
