48fe2e358811820ab2543ed9def5e0f00573a75b
Emma Roizot <emma.roizot@orange.fr>
2024-02-23 09:01:19 +0100

    mise en forme inscription

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,32 +1,34 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire de Connexion</title>
 </head>
+
 <body>
 
-<h1><a href="../index.php">Earthly</a></h1>
+    <h1><a href="../index.php">Earthly</a></h1>
 
-<a href="../index.php">Retour à l'index</a>
+    <a href="../index.php">Retour à l'index</a>
 
-<h2>Créer un compte</h2>
-<fieldset>
-<form action="../form/form_inscription.php" method="POST">
-    <label for="pseudo">Pseudo:</label><br>
-    <input type="text" id="pseudo" name="pseudo" required><br><br>
-    <label for="email">Adresse e-mail:</label><br>
-    <input type="email" id="email" name="email" required><br><br>
-    <label for="mdp">Mot de passe:</label><br>
-    <input type="password" id="mdp" name="mdp" required><br><br>
-    <label for="photo">Photo de profil:</label><br>
-    <input type="file" id="photo" name="photo"><br><br>
-    <input type="submit" value="Créer un compte">
-</form>
-</fieldset>
+    <h2>Créer un compte</h2>
+    <fieldset>
+        <form action="../form/form_inscription.php" method="POST">
+            <label for="pseudo">Pseudo:</label><br>
+            <input type="text" id="pseudo" name="pseudo" required><br><br>
+            <label for="email">Adresse e-mail:</label><br>
+            <input type="email" id="email" name="email" required><br><br>
+            <label for="mdp">Mot de passe:</label><br>
+            <input type="password" id="mdp" name="mdp" required><br><br>
+            <label for="photo">Photo de profil:</label><br>
+            <input type="file" id="photo" name="photo"><br><br>
+            <input type="submit" value="Créer un compte">
+        </form>
+    </fieldset>
 
-<!--
+    <!--
     Comptes : 
     AH - 1234
     admin - nice
@@ -34,4 +36,5 @@
 -->
 
 </body>
+
 </html>
