814fa34c44059bfde39a2029a650cec1413cf5fb
Baptiste Rouzic <rouzicb@gmail.com>
2024-02-23 09:34:04 +0100

    ajout style navbar

--- pages/classement.php
+++ pages/classement.php
@@ -59,12 +59,13 @@ $i = 0;
 
         if (isset($_SESSION['pseudo'])) {
         ?>
+        <ul class="footer-nav">
             <li><a href="planet.php">Ma Planète</a></li>
             <li><a href="defi.php">Mes défis journaliers</a></li>
             <li><a href="recyclage.php">Carte des poubelles</a></li>
             <li><a href="compte.php">Mon compte</a></li>
             <li><a href="classement.php">Classement</a></li>
-    </ul>
+        </ul>
 <?php
         }
 
