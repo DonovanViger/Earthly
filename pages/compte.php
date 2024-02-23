7b7c80ef1f23f13e58cc7e3bf628139d000fcc80
Tanguy Poulain <tanguy.poulain@univ-rouen.fr>
2024-02-23 09:59:41 +0100

    Remplacement de l'image de partage

--- pages/compte.php
+++ pages/compte.php
@@ -53,6 +53,7 @@ if (isset($_SESSION['pseudo'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <title>Mon compte</title>
 </head>
 
@@ -91,7 +92,8 @@ if (isset($_SESSION['pseudo'])) {
     ?>
 
     <br>
-    <img onclick="partager()" src="../image-partage.jpg" alt="">
+
+    <i onclick="partager()" class="fa-solid fa-share-nodes"></i>
 
     <script>
        
