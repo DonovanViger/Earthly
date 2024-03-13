80b0edf9b71d93ca1fe5c1d83667aaa9d42d02b5
Baptiste Rouzic <rouzicb@gmail.com>
2024-03-13 11:27:09 +0100

    css

--- pages/inscription.php
+++ pages/inscription.php
@@ -4,29 +4,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <title>Formulaire de Connexion</title>
 </head>
 
 <body>
 
-    <h1><a href="../index.php">Earthly</a></h1>
+    <h1 id="h1_inscription"><a href="../index.php">Earthly</a></h1>
 
-    <a href="../index.php">Retour à l'index</a>
 
-    <h2>Créer un compte</h2>
-    <fieldset>
+    <div id="inscription_cadre">
+    <h2 id="h2_inscription">Créer un compte</h2>
         <form action="../form/form_inscription.php" method="POST" enctype="multipart/form-data">
-            <label for="pseudo">Pseudo:</label><br>
+            <label for="pseudo">Pseudo:</label><br><br>
             <input type="text" id="pseudo" name="pseudo" required><br><br>
-            <label for="email">Adresse e-mail:</label><br>
+            <label for="email">Adresse e-mail:</label><br><br>
             <input type="email" id="email" name="email" required><br><br>
-            <label for="mdp">Mot de passe:</label><br>
+            <label for="mdp">Mot de passe:</label><br><br>
             <input type="password" id="mdp" name="mdp" required><br><br>
-            <label for="photo">Photo de profil:</label><br>
+            <label for="photo">Photo de profil:</label><br><br>
             <input type="file" id="photo" name="photo" accept="image/*" required><br><br>
-            <input type="submit" value="Créer un compte">
+            <br>
+            <input type="submit" value="Créer un compte" id="inscription_input_submit">
         </form>
-    </fieldset>
+</div>
+
+    <div id="inscription_retour">
+    <button id="inscription_button_retour">
+    <a href="../index.php">Retour à l'index</a>
+    </button>
+    </div>
 
     <!--
     Comptes : 
