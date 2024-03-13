0659c281770da520b66028febfc201b58b033837
Emma Roizot <emma.roizot@orange.fr>
2024-03-13 09:18:50 +0100

    ahah détresse

--- script/popup.js
+++ script/popup.js
@@ -1,7 +1,9 @@
 function afficherPopup() {
     document.getElementById("popup").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
 }
 
 function fermerPopup() {
     document.getElementById("popup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
 }
