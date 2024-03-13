c83bf8175a14a75fa13ef692f718d8a916851011
Baptiste Rouzic <rouzicb@gmail.com>
2024-03-13 10:32:04 +0100

    css

--- form/form_connexion.php
+++ form/form_connexion.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../pages/style.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+    <title>Earthly | Connexion</title>
+</head>
+<body>
+
+
+
+
 <?php
 session_start(); // Démarre la session (à placer au début de chaque fichier PHP où vous utilisez des sessions)
 
@@ -69,3 +83,8 @@ try {
     die("Erreur de connexion à la base de données : ". $erreur->getMessage());
 }
 ?>
+
+
+    
+</body>
+</html>
--- pages/compte.php
+++ pages/compte.php
@@ -121,6 +121,8 @@
     <div id="overlay"></div> <!-- Overlay pour l'arrière-plan semi-transparent -->
 
     <section id="profil">
+
+    <i onclick="partager()" class="fa-solid fa-share-nodes" id="compte_share"></i>
         <!-- Bouton pour afficher la pop-up -->
         <button onclick="afficherPopup()" id="compte_settings"><i class="fa-solid fa-gear"></i></button>
 
@@ -178,7 +180,7 @@
     
             <script src="../script/popup.js"></script>
 
-            <i onclick="partager()" class="fa-solid fa-share-nodes"></i>
+            
 
             <?php if (isset($_SESSION['pseudo'])) : ?>
     <footer>
