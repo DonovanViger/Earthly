2fad1edfe5e7d7f103670dd027e8b6e1c77a19ff
Baptiste Rouzic <rouzicb@gmail.com>
2024-03-13 11:06:22 +0100

    css

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+    <title>Earthly | Connexion</title>
+</head>
+<body>
+    
+
+
+
+
 <?php
 session_start(); // Démarre la session (à placer au début de chaque fichier PHP où vous utilisez des sessions)
 ?>
@@ -13,19 +28,29 @@ session_start(); // Démarre la session (à placer au début de chaque fichier P
 
 <body>
 
-    <h1><a href="../index.php">Earthly</a></h1>
+    <h1 id="h1_connexion"><a href="../index.php">Earthly</a></h1>
 
-    <a href="../index.php">Retour à l'index</a>
+    
 
-    <h2>Connexion</h2>
+    <div id="connexion_cadre">
+    <h2 id="h2_connexion">Connexion</h2>
     <form action="../form/form_connexion.php" method="POST">
         <label for="pseudo">Pseudo:</label><br>
+        </br>
         <input type="text" id="pseudo" name="pseudo" required><br><br>
         <label for="mdp">Mot de passe:</label><br>
+        </br>
         <input type="password" id="mdp" name="mdp" required><br><br>
+        </br>
         <input type="submit" value="Se connecter">
     </form>
+    </div>
 
+<div id="connexion_retour">
+    <button id="connexion_button_retour">
+    <a href="../index.php">Retour à l'index</a>
+    </button>
+</div>
 
 </body>
 
