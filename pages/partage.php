3f60ea833b9b314a232c5371bb7221cfd0b01b95
Tanguy Poulain <tanguy.poulain@univ-rouen.fr>
2024-03-19 09:47:47 +0100

    partage autre profil

--- pages/partage.php
+++ pages/partage.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css" />
     <title>Partage</title>
 </head>
 
@@ -22,11 +22,11 @@
         setlocale(LC_TIME, "fr_FR");
 
         // Récupération des informations de l'utilisateur connecté à partir de la session
-        $pseudo = $_SESSION['pseudo'];
+        $pseudoAutre = $_GET['pseudo'];
 
         // Requête SQL pour récupérer les informations de l'utilisateur
         $requete = $db->prepare("SELECT * FROM utilisateurs WHERE pseudo = :pseudo");
-        $requete->bindParam(':pseudo', $pseudo);
+        $requete->bindParam(':pseudo', $pseudoAutre);
         $requete->execute();
 
         // Récupération des résultats de la requête
