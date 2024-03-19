2c9ef4e7ae35226cf7ca6778dfd818002dfdd825
Donovan Viger <donovan.pro@outlook.fr>
2024-03-19 16:03:41 +0100

    ajustement from_connexion ajout/prete de point

--- form/form_connexion.php
+++ form/form_connexion.php
@@ -23,20 +23,55 @@ try {
         $_SESSION['pseudo'] = $pseudo;
         $_SESSION['mail'] = $utilisateur['mail']; // Ajoute l'adresse e-mail à la variable de session
         $_SESSION['user_id'] = $utilisateur['ID_Utilisateur'];
-        
+
         $db = new PDO('mysql:host=localhost;dbname=sae401-2', 'root', '');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         setlocale(LC_TIME, "fr_FR");
 
         $dateConnexion = date("Y-m-d");
+        $stmt_select_dateconnexion = $db->prepare("SELECT TIMESTAMPDIFF(DAY, dateConnexion, CURDATE()) AS jours_ecoules FROM utilisateurs WHERE `pseudo` = :pseudo");
+        $stmt_select_dateconnexion->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
+        $stmt_select_dateconnexion->execute();
+        $pointEnMoins = $stmt_select_dateconnexion->fetch(PDO::FETCH_ASSOC);
 
-        $query = $db->prepare("UPDATE utilisateurs SET dateConnexion = :dateConnexion WHERE pseudo = :pseudo");
 
-        // Liez les paramètres et exécutez la requête
+        if ($pointEnMoins) {
+            $jours_ecoules = $pointEnMoins['jours_ecoules'];
+            if ($jours_ecoules >= 1) {
+                $pointEnMoins = $jours_ecoules - 1;
+                $pointEnMoins = $pointEnMoins * -200;
+            } else {
+                $pointEnMoins = 0;
+            }
+        } else {
+            $pointEnMoins = 0;
+        }
+
+
+        $query = $db->prepare("UPDATE utilisateurs SET dateConnexion = :dateConnexion, point_Planete = point_Planete + :pointEnMoins WHERE pseudo = :pseudo");
         $query->bindParam(':dateConnexion', $dateConnexion, PDO::PARAM_STR);
         $query->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
+        $query->bindParam(':pointEnMoins', $pointEnMoins, PDO::PARAM_STR);
         $query->execute();
+
+
+        $stmt_select_point = $db->prepare("SELECT point_Planete FROM utilisateurs WHERE pseudo = :pseudo");
+        $stmt_select_point->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
+        $stmt_select_point->execute();
+        $pointActuelle = $stmt_select_point->fetch(PDO::FETCH_ASSOC);
+
+        // Assurez-vous de récupérer la valeur de point_Planete à partir du tableau
+        $pointActuelle = $pointActuelle['point_Planete'];
+
+        if ($pointActuelle < 0) {
+            $pointActuelle = 0;
+            $stmt_update_pointNeg = $db->prepare("UPDATE utilisateurs SET point_Planete = :pointActuelle WHERE pseudo = :pseudo");
+            $stmt_update_pointNeg->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
+            $stmt_update_pointNeg->bindParam(':pointActuelle', $pointActuelle, PDO::PARAM_STR);
+            $stmt_update_pointNeg->execute();
+        }
+
         header("Location: ../pages/compte.php"); // Redirige l'utilisateur vers la page d'accueil
         exit();
     } else {
@@ -45,28 +80,28 @@ try {
     }
     $date_actuelle = date('Y-m-d');
 
-        // Vérification si des défis sont sélectionnés pour la journée actuelle
-        $stmt_select_defis = $db->prepare("SELECT defis_journaliers.date, defiquotidien.nom, defiquotidien.desc FROM defis_journaliers INNER JOIN defiquotidien ON defis_journaliers.ID_Defi = defiquotidien.ID_Defi WHERE defis_journaliers.date = :date");
-        $stmt_select_defis->bindParam(':date', $date_actuelle);
-        $stmt_select_defis->execute();
-        $defis_journaliers = $stmt_select_defis->fetchAll(PDO::FETCH_ASSOC);
-        // Si la date du jour correspond à la colonne date de la table defis_journaliers
-        if (!empty($defis_journaliers)) {
-        } else {
-            echo "<a href='defi.php'>Rafraîchir la page pour voir les defis</a>";
-            $requete_defis = $db->query("SELECT * FROM defiquotidien ORDER BY RAND() LIMIT 3");
-            $defis_selectionnes = $requete_defis->fetchAll(PDO::FETCH_ASSOC);
+    // Vérification si des défis sont sélectionnés pour la journée actuelle
+    $stmt_select_defis = $db->prepare("SELECT defis_journaliers.date, defiquotidien.nom, defiquotidien.desc FROM defis_journaliers INNER JOIN defiquotidien ON defis_journaliers.ID_Defi = defiquotidien.ID_Defi WHERE defis_journaliers.date = :date");
+    $stmt_select_defis->bindParam(':date', $date_actuelle);
+    $stmt_select_defis->execute();
+    $defis_journaliers = $stmt_select_defis->fetchAll(PDO::FETCH_ASSOC);
+    // Si la date du jour correspond à la colonne date de la table defis_journaliers
+    if (!empty ($defis_journaliers)) {
+    } else {
+        echo "<a href='defi.php'>Rafraîchir la page pour voir les defis</a>";
+        $requete_defis = $db->query("SELECT * FROM defiquotidien ORDER BY RAND() LIMIT 3");
+        $defis_selectionnes = $requete_defis->fetchAll(PDO::FETCH_ASSOC);
 
-            // Insérer les défis sélectionnés dans la table des défis journaliers
-            $stmt_insert_defis = $db->prepare("INSERT INTO defis_journaliers (ID_Defi, date) VALUES (:id_defi, :date)");
-            foreach ($defis_selectionnes as $defi) {
-                $stmt_insert_defis->bindParam(':id_defi', $defi['ID_Defi']);
-                $stmt_insert_defis->bindParam(':date', $date_actuelle);
-                $stmt_insert_defis->execute();
-            }
+        // Insérer les défis sélectionnés dans la table des défis journaliers
+        $stmt_insert_defis = $db->prepare("INSERT INTO defis_journaliers (ID_Defi, date) VALUES (:id_defi, :date)");
+        foreach ($defis_selectionnes as $defi) {
+            $stmt_insert_defis->bindParam(':id_defi', $defi['ID_Defi']);
+            $stmt_insert_defis->bindParam(':date', $date_actuelle);
+            $stmt_insert_defis->execute();
         }
+    }
 } catch (PDOException $erreur) {
     // En cas d'erreur de connexion à la base de données
-    die("Erreur de connexion à la base de données : ". $erreur->getMessage());
+    die ("Erreur de connexion à la base de données : " . $erreur->getMessage());
 }
 ?>
