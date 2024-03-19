57b1b9c8c864e1986175019ddf6ac920a1ce861f
Emma Roizot <emma.roizot@orange.fr>
2024-03-19 10:13:59 +0100

    navbar temporaire

--- form/templates/footer.php
+++ form/templates/footer.php
@@ -4,14 +4,35 @@
         if (isset($_SESSION['pseudo'])) {
         ?>
 <footer>
-    <ul class="footer-nav">
-        <li><i class="fa-solid fa-qrcode"></i><a href="../pages/recyclage.php">Scanner</a></li>
-        <li><i class="fa-solid fa-earth-europe"></i><a href="../pages/planet.php">Planète</a></li>
-        <li><i class="fa-solid fa-trophy"></i><a href="../pages/defi.php">Défis journaliers</a></li>
-        <li><i class="fa-solid fa-medal"></i><a href="../pages/classement.php">Classement</a></li>
-        <li><i class="fa-solid fa-user"></i><a href="../pages/compte.php">Compte</a></li>
-    </ul>
+    <div class="nav">
+        <div class="nav-slot bg-white round-top-left">
+            <a href="../pages/defi.php" role="button" class="nav-link active">
+                <i class="fa-solid fa-trophy"></i>
+            </a>
+        </div>
+        <div class="nav-slot bg-white">
+            <a href="../pages/recyclage.php" class="nav-link">
+                <i class="fa-solid fa-qrcode"></i>
+            </a>
+        </div>
+        <div class="nav-slot curve">
+            <a href="../pages/planet.php" class="floating-button">
+                <i class="fa-solid fa-earth-europe"></i>
+            </a>
+        </div>
+        <div class="nav-slot bg-white">
+            <a href="../pages/classement.php" class="nav-link">
+                <i class="fa-solid fa-medal"></i>
+            </a>
+        </div>
+        <div class="nav-slot bg-white round-top-right">
+            <a href="../pages/compte.php" class="nav-link">
+                <i class="fa-solid fa-user"></i>
+            </a>
+        </div>
+    </div>
 </footer>
+
 <?php
         }
 
