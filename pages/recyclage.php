f216f4f0c33ff09c66031294ad6562dc0ba6ba0f
Emma Roizot <emma.roizot@orange.fr>
2024-03-19 08:52:52 +0100

    correction recyclage

--- pages/recyclage.php
+++ pages/recyclage.php
@@ -30,17 +30,17 @@
     <h1>Recyclez avec nos poubelles intelligentes</h1>
     <h2>Scannez un QR code.</h2>
 
+    <canvas id="canvas" style="display:none;"></canvas>
     <div id="recyclage_video">
-    <video id="video" width="500" height="400" autoplay></video>
-  </div>
-    <video id="video" width="400" height="300" autoplay></video>
+        <video id="video" width="500" height="400" autoplay></video>
+    </div>
     <div id="result"></div>
 
     <!-- Bouton pour activer/désactiver la caméra -->
     <div id="recyclage_button_box">
-    <button id="toggleButton" onclick="toggleCamera()">Activer/Désactiver la caméra</button>
-  </div>
-  
+        <button id="toggleButton">Activer/Désactiver la caméra</button>
+    </div>
+
     <?php 
     if (isset($_GET['poubelle'])) {
         $poubelle = $_GET['poubelle'];
@@ -58,7 +58,6 @@
     }
     ?>
 
-  </div>
     <?php include("../form/templates/footer.php"); ?>
 
     <script src="../node_modules/jsqr/dist/jsQR.js"></script>
@@ -66,27 +65,16 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
     <script src="bundle.js"></script>
     <script>
-
-document.addEventListener('DOMContentLoaded', function () {
-            var canvas = document.getElementById('canvas');
-
-            // Générer un QR code sur le canvas
-            new QRCode(canvas, {
-                text: 'sample text',
-                width: 150,
-                height: 150
-            });
-        });
-
     document.addEventListener('DOMContentLoaded', () => {
-        
-    const video = document.getElementById('video');
-    const context = canvas.getContext('2d');
 
-    let videoStream;
-    let captureInterval;
+        const video = document.getElementById('video');
+        const canvas = document.getElementById('canvas');
+        const context = canvas.getContext('2d');
 
-    const toggleCamera = () => {
+        let videoStream;
+        let captureInterval;
+
+        const toggleCamera = () => {
         if (videoStream) {
             stopCapture();
             document.getElementById('toggleButton').innerText = 'Activer la caméra';
@@ -96,54 +84,57 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     };
 
-    const startCapture = () => {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(stream => {
-                videoStream = stream;
-                video.srcObject = stream;
-                captureInterval = setInterval(captureAndDecode, 1000);
-            })
-            .catch(err => console.log("Erreur lors de l'accès à la webcam: " + err));
-    };
+    document.getElementById('toggleButton').addEventListener('click', toggleCamera);
 
-    const stopCapture = () => {
-        if (videoStream) {
-            videoStream.getTracks().forEach(track => track.stop());
-            clearInterval(captureInterval);
-            videoStream = null;
-        }
-    };
+        const startCapture = () => {
+            navigator.mediaDevices.getUserMedia({
+                    video: true
+                })
+                .then(stream => {
+                    videoStream = stream;
+                    video.srcObject = stream;
+                    captureInterval = setInterval(captureAndDecode, 1000);
+                })
+                .catch(err => console.log("Erreur lors de l'accès à la webcam: " + err));
+        };
 
-    const captureAndDecode = () => {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-        const code = jsQR(imageData.data, imageData.width, imageData.height);
-
-        if (code) {
-            const qrData = code.data;
-            document.getElementById('result').innerText = "QR Code trouvé : " + qrData;
-
-            if (isValidUrl(qrData)) {
-                stopCapture();
-                document.getElementById('toggleButton').innerText = 'Activer la caméra';
-                window.open(qrData, '_blank');
-
-                const button = document.createElement('button');
-                button.textContent = 'Visiter le site';
-                button.onclick = () => window.open(qrData, '_blank');
-                document.getElementById('result').innerHTML = '';
-                document.getElementById('result').appendChild(button);
+        const stopCapture = () => {
+            if (videoStream) {
+                videoStream.getTracks().forEach(track => track.stop());
+                clearInterval(captureInterval);
+                videoStream = null;
             }
-        }
-    };
+        };
 
-    const isValidUrl = url => /^(http|https):\/\/[\w\-]+(\.[\w\-]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?$/.test(url);
-});
+        const captureAndDecode = () => {
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            const code = jsQR(imageData.data, imageData.width, imageData.height);
+
+            if (code) {
+                const qrData = code.data;
+                document.getElementById('result').innerText = "QR Code trouvé : " + qrData;
+
+                if (isValidUrl(qrData)) {
+                    stopCapture();
+                    document.getElementById('toggleButton').innerText = 'Activer la caméra';
+                    window.open(qrData, '_blank');
+
+                    const button = document.createElement('button');
+                    button.textContent = 'Visiter le site';
+                    button.onclick = () => window.open(qrData, '_blank');
+                    document.getElementById('result').innerHTML = '';
+                    document.getElementById('result').appendChild(button);
+                }
+            }
+        };
+
+        const isValidUrl = url =>
+            /^(http|https):\/\/[\w\-]+(\.[\w\-]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?$/.test(url);
+    });
     </script>
 </body>
 
