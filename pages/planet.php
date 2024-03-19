de116b038012faa431d3e2a63000d8aaa73575f9
Tanguy Poulain <tanguy.poulain@univ-rouen.fr>
2024-03-19 14:14:56 +0100

    normalement c bon

--- pages/planet.php
+++ pages/planet.php
@@ -1,3 +1,31 @@
+<?php
+
+session_start(); // Démarre la session
+
+// Vérifie si l'utilisateur est connecté
+if(!isset($_SESSION['pseudo'])) {
+    // Redirige l'utilisateur vers la page de connexion s'il n'est pas connecté
+    header("Location: connexion.php");
+    exit();
+}
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=sae401-2', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $erreur) {
+    die("Erreur de connexion à la base de données : ". $erreur->getMessage());
+}
+
+// Requête SQL pour récupérer les informations de l'utilisateur
+$requete = $db->prepare("SELECT * FROM utilisateurs WHERE pseudo = :pseudo");
+$requete->bindParam(':pseudo', $pseudo);
+$requete->execute();
+
+// Récupération des résultats de la requête
+$utilisateur = $requete->fetch(PDO::FETCH_ASSOC);
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -30,27 +58,6 @@
 
 
 
-<?php
-
-session_start(); // Démarre la session
-
-// Vérifie si l'utilisateur est connecté
-if(!isset($_SESSION['pseudo'])) {
-    // Redirige l'utilisateur vers la page de connexion s'il n'est pas connecté
-    header("Location: connexion.php");
-    exit();
-}
-
-try {
-    $db = new PDO('mysql:host=localhost;dbname=sae401-2', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $erreur) {
-    die("Erreur de connexion à la base de données : ". $erreur->getMessage());
-}
-
-        // Affiche les liens "Se connecter" et "Créer un compte" seulement si l'utilisateur n'est pas connecté
-
-?>
 
 <?php
         include("../form/templates/footer.php")
