850e43a8af9f94fce88fb6177ff14b711c3ce6e2
Emma Roizot <emma.roizot@orange.fr>
2024-03-21 17:47:28 +0100

    AAAAH

--- form/templates/footer.php
+++ form/templates/footer.php
@@ -19,7 +19,7 @@
         </div>
         <div class="nav-slot curve">
             <a href="../pages/planet.php" class="floating-button" id="planet">
-                <img src="../img/nav bar/home_noir.png" class="footer-image" data-image="3.png">
+                <img src="../img/nav bar/home_noir.png" class="footer-image planet-icon" data-image="3.png">
             </a>
         </div>
         <div class="nav-slot">
@@ -36,6 +36,7 @@
         </div>
     </div>
 </footer>
+
 
 <?php
         }
