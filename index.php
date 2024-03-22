e4cf16dd4ced53b35b206f5f2b21d6bb49e2d310
Emma Roizot <emma.roizot@orange.fr>
2024-03-22 11:14:35 +0100

    Corrections finales du footer

--- index.php
+++ index.php
@@ -68,35 +68,39 @@
     <?php
     if (isset($_SESSION['pseudo'])) {
     ?>
-        <footer>
-            <div class="nav">
-                <div class="nav-slot round-top-left">
-                    <a href="pages/defi.php" role="button" class="nav-link" id="defi">
-                        <i class="fa-solid fa-trophy"></i>
-                    </a>
-                </div>
-                <div class="nav-slot">
-                    <a href="pages/recyclage.php" class="nav-link" id="planet">
-                    <i class="fa-solid fa-qrcode"></i>
-                    </a>
-                </div>
-                <div class="nav-slot curve">
-                    <a href="pages/planet.php" class="floating-button" id="recyclage">
-                        <i class="fa-solid fa-earth-europe"></i>
-                    </a>
-                </div>
-                <div class="nav-slot">
-                    <a href="pages/classement.php" class="nav-link" id="classement">
-                        <i class="fa-solid fa-medal"></i>
-                    </a>
-                </div>
-                <div class="nav-slot round-top-right">
-                    <a href="pages/compte.php" class="nav-link" id="compte">
-                        <i class="fa-solid fa-user"></i>
-                    </a>
-                </div>
-            </div>
-        </footer>
+<footer>
+    <div class="nav">
+        <div class="nav-slot">
+            <a href="pages/recyclage.php" class="nav-link" id="recyclage">
+                <img src="img/nav bar/Scanner noir.png" class="footer-image" data-image="2.png">
+                <span class="image-text">Recyclage</span>
+            </a>
+        </div>
+        <div class="nav-slot round-top-left">
+            <a href="pages/defi.php" class="nav-link" id="defi">
+                <img src="img/nav bar/Defis noir.png" class="footer-image" data-image="1.png">
+                <span class="image-text">Défis</span>
+            </a>
+        </div>
+        <div class="nav-slot">
+            <a href="pages/planet.php" class="nav-link" id="planet">
+                <img src="img/nav bar/home_noir.png" class="footer-image planet-icon" data-image="3.png">
+            </a>
+        </div>
+        <div class="nav-slot">
+            <a href="pages/classement.php" class="nav-link" id="classement">
+                <img src="img/nav bar/Classement noir.png" class="footer-image" data-image="4.png">
+                <span class="image-text">Classement</span>
+            </a>
+        </div>
+        <div class="nav-slot round-top-right">
+            <a href="pages/compte.php" class="nav-link" id="compte">
+                <img src="img/nav bar/Profil noir.png" class="footer-image" data-image="5.png">
+                <span class="image-text">Compte</span>
+            </a>
+        </div>
+    </div>
+</footer>
         <?php
         $pseudo = $_SESSION['pseudo'];
 
