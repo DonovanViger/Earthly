0e1291c2fb9cc288dbd31cfe6daf803c83bbb0f0
Emma Roizot <emma.roizot@orange.fr>
2024-03-22 11:53:02 +0100

    amélioration du classement (en cours)

--- pages/classement.php
+++ pages/classement.php
@@ -5,27 +5,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css" />
     <title>Classement</title>
     <style>
         .user-card {
             border-radius: 15px;
-            padding: 10px;
+            padding: 18px;
             margin-bottom: 10px;
             background-color: #2BBA7C;
             color: #FFEFE1;
+            font-weight: bold;
         }
 
         .user-avatar {
-            width: 59px;
+            width: 60px;
             border-radius: 50%;
         }
 
         .points-col {
-            border-left: 2px solid #FFFFFF;
-            border-radius: 1px;
+            border-left: 3px solid #FFFFFF;
+            border-radius: 2px;
+        }
+
+        #un {
+            background-color: #BAA32B;
+        }
+
+        #deux {
+            background-color: #98a09b;
+        }
+
+        #trois {
+            background-color: #AA5632;
+        }
+
+        .placement-large {
+            font-size: 32px;
+            /* Ajustez cette valeur selon vos besoins */
         }
     </style>
 </head>
@@ -52,8 +69,10 @@
 
     <div class="container">
         <?php foreach ($Classements as $key => $classement) { ?>
-            <div class="row user-card m-3">
-                <div class="col-1 text-center align-self-center"><?php echo $key + 1; ?></div>
+            <div class="row user-card mx-3 my-4" id="<?php if ($key == 0) echo 'un';
+                                                                        elseif ($key == 1) echo 'deux';
+                                                                        elseif ($key == 2) echo 'trois'; ?>">
+                <div class="col-1 text-center align-self-center placement-large"><?php echo $key + 1; ?></div>
                 <div class="col-4 text-center align-self-center">
                     <img src="<?php echo $classement['pdp']; ?>" alt="Avatar de <?php echo $classement['pseudo']; ?>" class="user-avatar">
                 </div>
@@ -66,8 +85,7 @@
     <?php
     include("../form/templates/footer.php")
     ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
 </body>
 
