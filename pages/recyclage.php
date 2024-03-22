4073a8d29f4f92244c0c6cf47c690c07c04c2e91
Tanguy Poulain <tanguy.poulain60@gmail.com>
2024-03-22 17:13:11 +0100

    scanner carré milieu

--- pages/recyclage.php
+++ pages/recyclage.php
@@ -53,6 +53,15 @@
     }
     ?>
 
+    <div style="position:absolute;left:0px;top:0px;width:100vw;height:100vh;z-index:1;">
+        <div style="display:flex;justify-content:center;align-items:center;width:100vw;height:100vh;">
+            <svg width="200" height="200">
+                <rect width="200" height="200" stroke="white" stroke-width="10" rx="50" ry="50" fill-opacity="0" />
+            </svg>
+        </div>
+    </div>
+
+
     <?php include("../form/templates/footer.php"); ?>
 
     <script src="../node_modules/jsqr/dist/jsQR.js"></script>
