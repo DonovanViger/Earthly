b3a65e24ed5f8c26e3b06f5b3ef5844325bb8791
Tanguy Poulain <tanguy.poulain60@gmail.com>
2024-03-25 16:46:25 +0100

    rien

--- pages/success.php
+++ pages/success.php
@@ -1,3 +1,14 @@
+<?php
+session_start(); // Démarre la session
+
+// Vérifie si l'utilisateur est connecté
+if (!isset($_SESSION['pseudo'])) {
+    // Redirige l'utilisateur vers la page de connexion s'il n'est pas connecté
+    header("Location: connexion.php");
+    exit();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,6 +18,30 @@
     <title>Earthly | Succès</title>
 </head>
 <body>
+    <?php
+    $db = new PDO('mysql:host=localhost;dbname=sae401-2', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $select_succes_user = $db->prepare("SELECT ID_Succes, dateObtention FROM utilisateursucces where ID_Utilisateur=:id_utilisateur");
+    $select_succes_user->bindParam(':id_utilisateur', $_SESSION['user_id']);
+    $select_succes_user->execute();
+    $succesuser = $select_succes_user->fetchAll();
+    $tableau = [1 => "", 2 => "", 3 => "", 4 => "", 5 => "", 6 => "", 7 => "", 8 => "", 9 => "", 10 => "", 14 => "", 15 => "", 16 => "", 17 => "", 18 => "", 19 => "", 20 => "", 21 => "", ];
+
+    echo "<script> console.table(".json_encode($succesuser).");</script>";
+    echo "<script> console.table(".json_encode($tableau).");</script>";
+
+    $nn = 1;
+    foreach ($succesuser as $succesnumber) {
+        echo "<script> console.log(".json_encode($succesnumber[1]).");</script>";
+        if ($succesnumber[1] != "0000-00-00"){
+            $tableau[$nn]="oui";
+        }
+        $nn++;
+    }
+
+    echo "<script> console.table(".json_encode($tableau).");</script>";
+    ?>
 
     <div id="success_overall">
         <div id="success_title_box">
@@ -23,7 +58,10 @@
   <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0" />
     </svg>
     <h2 id="success_h2_1">Petite branche</h2>
-    <div id="success_number"></div>
+    <div id="success_number">
+        <?php 
+        ?>
+    </div>
 
     <div class="success_box_1">
         <h3 class="success_h3_darkgreen">Petite branche <strong class="succes_strong_green">I</strong></h3>
