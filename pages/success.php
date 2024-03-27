2130b20616b6a32e879f542f828011156655bc1b
Emma Roizot <emma.roizot@orange.fr>
2024-03-27 18:20:32 +0100

    hehe

--- pages/success.php
+++ pages/success.php
@@ -126,7 +126,7 @@ if (!isset($_SESSION['pseudo'])) {
         <div class="success_advencement"><?php if ($tableau[2] == "oui") {
             echo "100%";
         } else if (isset($tableau[2])) {
-            $calcul = $tableau[2]*20;
+            $calcul = intval($tableau[2])*20;
             echo $calcul;
             echo "%";
         } else {
