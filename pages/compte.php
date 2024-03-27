7cdf5927749f4c150a0119c9b3186483e15ca613
Donovan Viger <donovan.pro@outlook.fr>
2024-03-27 11:22:51 +0100

    ajustement succes badge

--- pages/compte.php
+++ pages/compte.php
@@ -353,7 +353,7 @@
                                         // Exemple de requête SQL pour récupérer le succès pour chaque slot
                                         $requete_succes = $db->prepare("SELECT s.ID_Succes, s.pds, s.nom FROM succes s 
                                         INNER JOIN utilisateursucces us ON s.ID_Succes = us.ID_Succes 
-                                        WHERE us.ID_Utilisateur = :id_utilisateur AND s.triageSucces LIKE :group AND us.dateObtention != 00-00-0000
+                                        WHERE us.ID_Utilisateur = :id_utilisateur AND s.triageSucces LIKE :group AND us.dateObtention != 01-01-1999
                                         ORDER BY CAST(SUBSTRING(s.triageSucces, 2) AS UNSIGNED) DESC
                                         LIMIT 1");
                                         $requete_succes->bindParam(':id_utilisateur', $user_id);
