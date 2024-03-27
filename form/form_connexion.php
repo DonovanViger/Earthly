8866c856e7e35dc2e1e211bfb6f961aa4a3b61ec
Emma Roizot <emma.roizot@orange.fr>
2024-03-27 13:59:18 +0100

    qsdqs

--- form/form_connexion.php
+++ form/form_connexion.php
@@ -12,27 +12,22 @@
 </html>
 
 <?php
-session_start(); // Démarre la session (à placer au début de chaque fichier PHP où vous utilisez des sessions)
+session_start();
 
 try {
-    // Connexion à la base de données
     $db = new PDO('mysql:host=localhost;dbname=sae401-2', 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Récupération des données du formulaire
     $pseudo = $_POST['pseudo'];
     $mdp = hash('sha256', $_POST['mdp']);
 
-    // Requête SQL pour vérifier les informations de connexion
     $requete = $db->prepare("SELECT * FROM utilisateurs WHERE pseudo = :pseudo AND mdp = :mdp");
     $requete->bindParam(':pseudo', $pseudo);
     $requete->bindParam(':mdp', $mdp);
     $requete->execute();
 
-    // Vérification des résultats de la requête
     $utilisateur = $requete->fetch(PDO::FETCH_ASSOC);
     if ($utilisateur) {
-        // Crée une variable de session pour l'utilisateur connecté
         $_SESSION['pseudo'] = $pseudo;
         $_SESSION['mail'] = $utilisateur['mail']; // Ajoute l'adresse e-mail à la variable de session
         $_SESSION['user_id'] = $utilisateur['ID_Utilisateur'];
@@ -135,7 +130,6 @@ try {
         $requete_defis = $db->query("SELECT * FROM defiquotidien ORDER BY RAND() LIMIT 3");
         $defis_selectionnes = $requete_defis->fetchAll(PDO::FETCH_ASSOC);
 
-        // Insérer les défis sélectionnés dans la table des défis journaliers
         $stmt_insert_defis = $db->prepare("INSERT INTO defis_journaliers (ID_Defi, date) VALUES (:id_defi, :date)");
         foreach ($defis_selectionnes as $defi) {
             $insert_defis->bindParam(':id_defi', $defi['ID_Defi']);
@@ -144,7 +138,7 @@ try {
         }
     }
 
-        header("Location: ../pages/compte.php"); // Redirige l'utilisateur vers la page d'accueil
+        header("Location: ../pages/compte.php");
         exit();
     } else {
         echo "<div id='erreur_connexion_box'>";
